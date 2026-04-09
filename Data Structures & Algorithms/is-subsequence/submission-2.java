7670b9f747fe0130e560d6d6fd1df56dbcddb66a
Mudassir Ansari <65652074+mudassiransari10@users.noreply.github.com>
2026-04-09 23:10:44 +0530

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() == 0) {
+            return true;
+        }
+        int j=0;
+        for(int i=0; i<t.length(); i++) {
+            if(j == s.length()) {
+                return true;
+            }
+            if(s.charAt(j) == t.charAt(i)) {
+                j++;
+            }
+        }
+        return j == s.length();
+    }
+}
